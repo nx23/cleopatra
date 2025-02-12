3d173dce465c8ed9bf3e093bc1084953d6f45956
Gabriel Botareli <gbotareli@gmail.com>
2025-02-12 18:57:29 -0300

    feat: update App.vue to include TabsWrapper and Tab components, and modify background color in style.css

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,26 @@
 <script setup>
-  import Logo from './components/Logo.vue';
-  import Prolabore from './components/Prolabore.vue';
+  import Logo from "./components/Logo.vue";
+  import TabsWrapper from "./components/TabsWrapper.vue";
+  import Tab from "./components/Tab.vue";
+  // import Prolabore from "./components/Prolabore.vue";
 </script>
 
-<template>
-  <Prolabore/>
+<template class="container">
+  <!-- <Prolabore/> -->
+  <TabsWrapper>
+    <Tab title="Menu 1">Hello from Tab 1</Tab>
+    <Tab title="Menu 2">Hello from Tab 2</Tab>
+    <Tab title="Menu 3">Hello from Tab 3</Tab>
+  </TabsWrapper>
   <Logo/>
 </template>
 
-
+<style scoped>
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+  }
+</style>
